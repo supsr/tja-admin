3cabd6675d25b02b42264894b39c98d1f2ee798b
Pawarit Saengpreecharutn <su.psr.92@gmail.com>
2019-04-03 00:13:03 +0700

    Remove asset files

--- assets/firebase/init.js
+++
@@ -1,9 +0,0 @@
-if (typeof firebase === 'undefined') throw new Error('hosting/init-error: Firebase SDK not detected. You must include it before /__/firebase/init.js');
-firebase.initializeApp({
-    apiKey: "AIzaSyC0kIyNRX0_h7PAvlivr3on2DXbLJH1u-M",
-    authDomain: "thaijogging-214013.firebaseapp.com",
-    databaseURL: "https://thaijogging-214013.firebaseio.com",
-    projectId: "thaijogging-214013",
-    storageBucket: "thaijogging-214013.appspot.com",
-    messagingSenderId: "159830461119"
-});
